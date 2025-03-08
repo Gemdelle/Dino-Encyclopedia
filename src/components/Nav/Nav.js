b25040a3e4ac72fd6e76fd0aa5716c24dd59299d
Hunny <unreal5_gonza@outlook.es>
2025-03-08 19:43:27 -0300

    Arregla el /home del Nav.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
       <nav>
           <div className="profile-container"></div>
           <div className="routes">
-              <Link to="/home">Home</Link>
+              <Link to="/">Home</Link>
               <Link to="/map">Map</Link>
               <Link to="/encyclopedia">Encyclopedia</Link>
               <Link to="/album">Album</Link>
